ed544093e49afe97aca3f2999b6c3798646486f4
devnio <nihat_95@hotmail.it>
2022-02-21 14:56:44 +0100

    update

--- script.js
+++ script.js
@@ -1,95 +1,161 @@
-// // Utility delay function
-// async function delay(delayInms) {
-//     return new Promise(resolve  => {
-//       setTimeout(() => {
-//         resolve(2);
-//       }, delayInms);
-//     });
-// }
+function wrapLettersInSpan() {
+    console.log("Every word into span");
 
-// // Wrap every letter in a span
-// async function wrapTextForAnimation() {
-//     let delayTime = 1000; // 1s
-//     let delayres = await delay(delayTime);
+    var textWrapper = document.querySelectorAll('.slide-text');
+    
+    if (textWrapper != null) {
+        textWrapper.forEach(element => {
+            console.log("Converting .slide-text sentences into letters captured by span");
+            element.innerHTML = element.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+        });
+    }
+}
 
-//     console.log("Animation script running!");
+function animate() {
+    anime.timeline({
+        loop: false,
+        complete: function(anim) {
+            if (anim.began && anim.completed) {
+                // todo
+            }
+        },
+    })
+    .add({
+        targets: '.slide-text',
+        opacity: 1,
+        easing: "easeInOutQuad",
+        duration: 1,
+    })
+    .add({
+        targets: '.slide-text .letter',
+        opacity: [0,1],
+        easing: "easeInOutQuad",
+        duration: 1500,
+        delay: (el, i) => 30 * (i+1)
+    })
+    .add({
+        targets: '.emoji',
+        opacity: [0,1],
+        easing: "easeInOutQuad",
+        duration: 400,
+    })
+}
 
-//     var textWrapper = document.querySelector('.slide-text');
-//     if (textWrapper != null) {
-//         console.log("Found .slide-text element");
+function aniamteOut() {
+    anime.timeline({
+        loop: false,
+        complete: function(anim) {
+            if (anim.began && anim.completed) {
+                let content = getNextContent();
+                changeTextContent(content);
+                wrapLettersInSpan();
+                changeEmojiContentRandom();
+                animate();
+            }
+        },
+    })
+    .add({
+        targets: '.slide-text',
+        opacity: 0,
+        duration: 500,
+        easing: "easeOutExpo",
+        delay: 50
+    })
+    .add({
+        targets: '.emoji',
+        opacity: 0,
+        easing: "easeInOutQuad",
+        duration: 200,
+    });
+}
 
-//         textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-//     }
-// }
+function changeTextContent(content) {
+    const textEl = document.getElementsByClassName("slide-text");
+    textEl[0].innerHTML = content;
+}
 
-// wrapTextForAnimation();
+function changeEmojiContentRandom() {
+    const emoj = document.getElementsByClassName("emoji");
+    emoj[0].innerHTML = emojis[getRandomIndex(emojis)];
+}
+
+function getRandomIndex(arr) {
+    return Math.floor(Math.random()*arr.length);
+}
+
+function getNextContent() {
+
+    console.log("Next content: " + contentIdx);
+
+    // Fill array indices again if all elements are already shown
+    if (contentIdx.length == 0) {
+        contentIdx = Array.from(Array(contentText.length).keys());
+    }
+
+    // Get random index from content indices and delete from array (no duplicate show)
+    let randomIdx = getRandomIndex(contentIdx);
+    let content = contentText[contentIdx[randomIdx]];
+    contentIdx.splice(randomIdx, 1); // 2nd parameter means remove one item only
+
+    // Return content
+    return content;
+}
 
 $(document).ready(function () {
-    // console.log("Document ready!");
+    console.log("Document ready!");
 
-    // console.log("Every word into span");
-    // var textWrapper = document.querySelectorAll('.slide-text');
-    
-    // console.log(textWrapper);
+    const btn = document.getElementById('button');
+    btn.onclick = function(){
+        aniamteOut();
+    };
 
-    // if (textWrapper != null) {
-    //     textWrapper.forEach(element => {
-    //         console.log("Converting .slide-text sentences into letters captured by span");
-    //         element.innerHTML = element.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-    //     });
-    // }
+    wrapLettersInSpan();
 
-    // let index2class = {0: '.first', 1: '.second', 2: '.third'};
+    animate();
 
-    // $('#myCarousel').on('slide.bs.carousel', function (e) {
-        // console.log("Slide finished!");
-        
-        // var prevSlide = index2class[e.from];
-        // var currSlide = index2class[e.to];
-        
-        // console.log("Prev slide: " + prevSlide);
-        // console.log("Curr slide: " + currSlide);
-        
-        // let targetsToHide0 = '.slide-text' + prevSlide;
-        // let targetsToHide1 = '.slide-text' + prevSlide + ' .letter';
-        
-        // let targetsToShow0 = '.slide-text' + currSlide;
-        // let targetsToShow1 = '.slide-text' + currSlide + ' .letter';
-
-        // anime.timeline({loop: false})
-        // .add({
-        //     targets: targetsToHide0,
-        //     opacity: [0],
-        //     easing: "easeInOutQuad",
-        //     duration: 1,
-        // })
-        // .add({
-        //     targets: targetsToHide1,
-        //     opacity: [0],
-        //     easing: "easeInOutQuad",
-        //     duration: 1,
-        // })
-
-        // .add({
-        //     targets: targetsToShow0,
-        //     opacity: [0,1],
-        //     easing: "easeInOutQuad",
-        //     duration: 100,
-        // })
-        // .add({
-        //     targets: targetsToShow1,
-        //     opacity: [0,1],
-        //     easing: "easeInOutQuad",
-        //     duration: 2250,
-        //     delay: (el, i) => 150 * (i+1)
-        // }); 
-        // .add({
-        //     targets: '.slide-text',
-        //     opacity: 0,
-        //     duration: 1000,
-        //     easing: "easeOutExpo",
-        //     delay: 1000
-        // });
-    // })
+    // const textEl = document.getElementsByClassName("slide-text");
+    // textEl[0].innerHTML = "c"
 
 });
+
+var contentText = [
+    "You deserve to be happy!",
+    "You are beautiful!",
+    "You are worthy to be loved!",
+    "Love is all around you wherever you go!",
+    "You are complete as you are, others simply support you!",
+    "You are kind!",
+    "Your smile is gorgeus!",
+    "You shouldn't change for anyone!",
+    "You are perfect as you are!",
+    "You are loved and worthy!",
+    "You are safe and surrounded by love and support!",
+    "It's ok to make mistakes. We always learn!",
+    "You are understood and your perspective is important!",
+    "You are valued and helpful!",
+    "You celebrate the good qualities in others and yourself!",
+    "You have everything you need to succeed!",
+    "You uplift your joy and the joy of others!",
+    "Your feelings deserve names, deserve recognition, deserve to be felt!",
+    "Your perspective is unique and important!",
+    "Your pleasure does not require someone else's pain!",
+    "Your sensitivity is beautiful, and your feelings and emotions are valid!",
+    "There is something in this world that only you can do. That is why you are here!",
+];
+
+var contentIdx = Array.from(Array(contentText.length).keys());
+
+var emojis = [
+    '✨',
+    '❤️',
+    '❣️',
+    '💝',
+    '👸',
+    '🐨',
+    '💖',
+    '🍀',
+    '😇',
+    '💗',
+    '🙊',
+    '🙏'
+];
